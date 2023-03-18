e75d55a578abcd4f1e6a69bc7918e36191ca839e
amirgit92 <amir.babazadeh90@gmail.com>
2023-03-19 00:19:21 +0330

    jpa entities with relationships

---
+++ src/main/java/com/amir/spring6learning/Spring6learningApplication.java
@@ -0,0 +1,13 @@
+package com.amir.spring6learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6learningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6learningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amir/spring6learning/Spring6learningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amir.spring6learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6learningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
