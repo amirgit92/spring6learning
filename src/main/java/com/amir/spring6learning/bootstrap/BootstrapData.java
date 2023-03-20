06b42a241c743d1d107bc6bc4848bdcf9e31af71
amirgit92 <amir.babazadeh90@gmail.com>
2023-03-20 16:06:37 +0330

    Publisher added bug fixed

--- src/main/java/com/amir/spring6learning/bootstrap/BootstrapData.java
+++ src/main/java/com/amir/spring6learning/bootstrap/BootstrapData.java
@@ -29,14 +29,15 @@ public class BootstrapData implements CommandLineRunner {
         Author author2 = new Author();
         author1.setFirstName("Eric");
         author1.setLastName("Evans");
-        author2.setLastName("Rod");
+        author2.setFirstName("Rod");
         author2.setLastName("Johnson");
+        Author eric = authorRepository.save(author1);
+        Author rod = authorRepository.save(author2);
 
         Publisher publisher = new Publisher();
         publisher.setPublisherName("my publisher");
         publisher.setAddress("a random address");
         Publisher savedPublisher = publisherRepository.save(publisher);
-
 
         Book book1 = new Book();
         Book book2 = new Book();
@@ -46,15 +47,18 @@ public class BootstrapData implements CommandLineRunner {
         book2.setTitle("J2EE Development without EJB");
         book2.setIsbn("654321");
         book2.setPublisher(savedPublisher);
-
-
-        Author eric = authorRepository.save(author1);
-        Author rod = authorRepository.save(author2);
         Book book1Saved = bookRepository.save(book1);
         Book book2Saved = bookRepository.save(book2);
 
         eric.getBooks().add(book1Saved);
         rod.getBooks().add(book2Saved);
+        book1Saved.getAuthors().add(eric);
+        book2Saved.getAuthors().add(rod);
+
+        authorRepository.save(eric);
+        authorRepository.save(rod);
+        bookRepository.save(book1);
+        bookRepository.save(book2);
 
         System.out.println("In Bootstrap");
         System.out.println("Author Count: " + authorRepository.count());
